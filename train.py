1d180591318088a5ceb745eebf38d412369c6472
Krushal Kalkani <kkalkani2024@fau.edu>
2026-03-25 18:30:33 -0400

    trained the logistic model

--- train.py
+++ train.py
@@ -1,11 +1,11 @@
 import numpy as np
+from sklearn.linear_model import LogisticRegression
 
 X_train = np.load('X_train.npy')
 X_test = np.load('X_test.npy')
 y_train = np.load('y_train.npy')
 y_test = np.load('y_test.npy')
 
-print(X_train.shape)
-print(X_test.shape)
-print(y_train.shape)
-print(y_test.shape)
+# Traing logistic model on the training set
+classifier = LogisticRegression(random_state=42)
+classifier.fit(X_train, y_train)
