9fd5fa797e8e4c47650d7494c886ad01152f9375
Krushal Kalkani <kkalkani2024@fau.edu>
2026-03-25 14:30:26 -0400

    split the dataset and featurscaling is done

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,7 @@
 import pandas as pd
-from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 dataset = pd.read_csv('heart_attack_prediction_dataset.csv')
 dataset.drop(['Patient ID', 'Country', 'Continent',
@@ -28,5 +28,24 @@ dataset['Sex'] = dataset['Sex'].map({'Male': 0, 'Female': 1})
 dataset = pd.get_dummies(
     dataset, columns=['Diet'], drop_first=True, dtype=int, prefix='Diet')
 
-print(dataset.head())
-print(dataset.dtypes)
+# print(dataset.head())
+# print(dataset.dtypes)
+
+# Separate features and target variable
+X = dataset.drop('Heart Attack Risk', axis=1).values
+y = dataset['Heart Attack Risk'].values
+
+# Split the dataset into training and testing sets
+
+X_train, X_test, y_train, y_test = train_test_split(
+    # "it ensures reproducibility — the specific number is arbitrary
+    X, y, test_size=0.2, random_state=42)
+
+
+# Feature scaling
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+print(X_train.shape)
+print(X_test.shape)
