c5ecb5802430e5334181e42a47701df57d5aee06
Krushal Kalkani <kkalkani2024@fau.edu>
2026-03-25 18:41:22 -0400

    evaluate the model

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, roc_auc_score
 
 X_train = np.load('X_train.npy')
 X_test = np.load('X_test.npy')
@@ -7,5 +8,10 @@ y_train = np.load('y_train.npy')
 y_test = np.load('y_test.npy')
 
 # Traing logistic model on the training set
-classifier = LogisticRegression(random_state=42)
+classifier = LogisticRegression(max_iter=1000, random_state=42)
 classifier.fit(X_train, y_train)
+
+# Evaluating the model on the test set
+y_pred = classifier.predict(X_test)
+print(classification_report(y_test, y_pred))
+print("ROC AUC Score:", roc_auc_score(y_test, y_pred))
